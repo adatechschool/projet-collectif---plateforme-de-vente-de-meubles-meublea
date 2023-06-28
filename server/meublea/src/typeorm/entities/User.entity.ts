e7bc33fa800fba8b9c979384f9b17d8a70af51c4
TheUtopy <109255677+TheUtopy@users.noreply.github.com>
2023-06-28 14:26:46 +0200

    Creation of the user table

---
+++ server/meublea/src/typeorm/entities/User.entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name:'user'})
+export class User{
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({type: "varchar", length: 45})
+    username: string;
+
+    @Column({type: "varchar", length: 45})
+    mail: string;
+
+    @Column()
+    password: string;
+
+    @Column({type: "varchar", length: 15, nullable: true})
+    phone_number: string;
+
+    @Column({type: "varchar", length: 45, nullable: true})
+    first_name: string;
+
+    @Column({type: "varchar", length: 45, nullable: true})
+    last_name: string;
+
+    @Column({type: "varchar", length: 45, nullable: true})
+    adress: string;
+}
