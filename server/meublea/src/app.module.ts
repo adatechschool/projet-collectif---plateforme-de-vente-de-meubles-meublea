b4e31a40002c0d24b92d1cfb367f98a020c55dfa
François <fdiouf696@gmail.com>
2023-06-29 10:09:43 +0200

    modification du localhost

--- server/meublea/src/app.module.ts
+++ server/meublea/src/app.module.ts
@@ -8,7 +8,7 @@ import { DataSourceOptions } from 'typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: '127.0.0.1',
       port: 3306,
       username: 'root',
       password: '1234',
