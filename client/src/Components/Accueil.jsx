85b2b30a9c20f6b4976ebf1029ecf9ef5978d250
FlorianLUSSON <florianlusson@gmail.com>
2023-06-29 16:06:07 +0200

    Add Header + links ok

---
+++ client/src/Components/Accueil.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Accueil = () => {
+    return (
+        <h1>Accueil</h1>
+    );
+}
+
+export default Accueil;
---
+++ client/src/Components/Connexion.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Connexion = () => {
+    return (
+        <h1>Connexion</h1>
+    );
+}
+
+export default Connexion;
