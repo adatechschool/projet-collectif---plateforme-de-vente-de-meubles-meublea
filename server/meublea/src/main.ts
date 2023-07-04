ef485d78ed44b81c1056c48d6f12f100e2de5196
TheUtopy <109255677+TheUtopy@users.noreply.github.com>
2023-07-04 16:02:45 +0200

    Feature : connexion API is now handled Navbar is different wether user is connected or not. Admin connexion is also handle. Use of cookies for that. Admin button is a dead link for now. If user is logged in, can't go to Connexion or Inscription pages.

--- server/meublea/src/main.ts
+++ server/meublea/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(cookieParser());
+
+  const corsOptions: CorsOptions = {
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
+  
   await app.listen(3000);
 }
+
 bootstrap();
